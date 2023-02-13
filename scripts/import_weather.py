32f3fbff12161ffd19039cddc5197665e342135a
Arti Jariwala <116127790+aarti21388@users.noreply.github.com>
2023-02-13 02:12:39 -0800

    Update import_weather.py

--- scripts/import_weather.py
+++ scripts/import_weather.py
@@ -8,15 +8,15 @@ path=f"{os.getcwd()}/wx_data"
 
 def read_text_file(files):
     for f in files:
-        file_path=f"{path}/{f}"
+        file_path=f"{path}/{f}" # file name will store in file_path variable
         with open(file_path,"r") as data_read:
-            lines=[l.rstrip() for l in data_read]
+            lines=[l.rstrip() for l in data_read] # all lines from .txt file store in lines variable
             for line in lines:
-                data=line.split('\t')
+                data=line.split('\t') # data is separted by \t which will split it and store in data list
                 w=Weather.objects.create(station=f[:-4],weather_date=int(data[0]),maximum_temperature=int(data[1]),minimum_temperature=int(data[2]),precipitation=int(data[3]))
-                w.save()
+                w.save() # saving to the weather model with station Id, date,tempreture and precipitation
         
-        shutil.move(file_path,f"{os.getcwd()}/process_wx_data/{f}")
+        shutil.move(file_path,f"{os.getcwd()}/process_wx_data/{f}") #move process file to different folder
     
 read_text_file(os.listdir(path))
 
